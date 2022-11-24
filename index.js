b28fe3719c04e3603835b370b7a2c18fe64e74ed
anichu <anis.cse5.bu@gmail.com>
2022-11-24 19:47:43 +0600

    initialize the server

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
+// middleware
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_DEV === "development") {
+	app.use(morgan("dev"));
+}
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wjvzlqr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+function verifyJwt(req, res, next) {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).send({ message: "Unauthorized access" });
+	}
+
+	const token = authHeader.split(" ")[1];
+	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+		if (err) {
+			res.status(403).send({ message: "Forbidden access" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+}
+
+async function run() {
+	try {
+		const phonesCollection = client.db("resealPhone").collection("phones");
+		const usersCollection = client.db("resealPhone").collection("users");
+		app.get("/jwt", async (req, res) => {
+			const email = req.query.email;
+			const query = {
+				email: email,
+			};
+			const user = await usersCollection.findOne(query);
+			console.log("jwt", user);
+			let token;
+			if (user) {
+				token = jwt.sign({ email }, process.env.JWT_SECRET, {
+					expiresIn: "7d",
+				});
+				return res.send({ accessToken: token });
+			}
+
+			res.status(403).send({ accessToken: token });
+		});
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+run().catch((err) => console.log(err));
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
